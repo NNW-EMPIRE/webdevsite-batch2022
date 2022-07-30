246d915037f1a60c2b76a8e34937adc45a30e872
Earl Millen Toh Akwe <69284908+akwe-afriitech@users.noreply.github.com>
2022-07-30 22:26:18 +0100

    Add files via upload

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,4 +1,20 @@
+
+
+(function($) {
+  "use strict";
+
 function menu(){
   var menuli = "this is nnw empire where dreams come true";
    document.getElementById("p").innerHTML= menuli ;
 }
+var typed = new Typed('.typed-word', {
+  strings: ["Web Developer","Softwware Developer"," Security Analyst", "UI / UX", "Wed Development Trainer"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 2000,
+  startDelay: 1500,
+  loop: true,
+  showCursor: true
+});
+       
+})(jQuery);
